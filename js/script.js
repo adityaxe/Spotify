bff2991ec50b9340cf236c63044b2259c7c21db7
adityaxe <adityabalhra@gmail.com>
2025-04-23 12:27:38 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,22 +13,6 @@ async function getSongs() {
 }
 
 
-
-// async function getSongs() {
-//     let a = await fetch(`/${folder}`)
-//     let response = await a.text();
-//     let div = document.createElement("div")
-//     div.innerHTML = response;
-//     let as = div.getElementsByTagName("a")
-//     songs = []
-//     for (let index = 0; index < as.length; index++) {
-//         const element = as[index];
-//         if (element.href.endsWith(".mp3")) {
-//             songs.push(element.href.split(`/${folder}/`)[1])
-//         }
-//     }
-//     return (songs)
-// }
 function secondsToMinutesSeconds(seconds) {
     if (isNaN(seconds) || seconds < 0) {
         return "00:00";
